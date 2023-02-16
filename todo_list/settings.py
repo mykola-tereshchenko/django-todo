43773834f02e9ae44b417ae23f71003e769f71b2
mykola-tereshchenko <tereshchenko.nikolay@gmail.com>
2023-02-16 14:37:15 +0200

    Register new users

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static/']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
