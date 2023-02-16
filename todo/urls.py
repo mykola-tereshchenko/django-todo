1e67fdec15a449e9ef72786f1ea4086ce4b1d1b3
mykola-tereshchenko <tereshchenko.nikolay@gmail.com>
2023-02-16 13:50:05 +0200

    Add form for working with task

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from .views import index, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('tasks/', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/create/', TaskCreate.as_view(), name = 'task-create'),
+    path('task/update/<int:pk>/', TaskUpdate.as_view(),name='task-update'),
+    path('task/delete/<int:pk>/', TaskDelete.as_view(),name='task-delete'),
 ]
